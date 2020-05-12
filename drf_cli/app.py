38fff45d546c3780bcb008402ff65322ee9535b7
Patrick <patrick.pwall@gmail.com>
2020-05-12 17:14:34 -0300

    Implements function to get cookiecutters path

--- drf_cli/app.py
+++ drf_cli/app.py
@@ -1,3 +1,5 @@
+import os
+
 import typer
 from cookiecutter.main import cookiecutter
 
@@ -7,17 +9,27 @@ new_app = typer.Typer()
 app.add_typer(new_app, name="new")
 
 
+def get_cookiecutter_path(name: str) -> str:
+    script_path = os.path.abspath(__file__)
+    module_path = os.path.dirname(script_path)
+    project_path = os.path.dirname(module_path)
+    cookiecutters_path = os.path.join(project_path, "cookiecutters")
+    return os.path.join(cookiecutters_path, name)
+
+
 @new_app.command("project")
 def new_project():
     typer.echo("Creating new project...")
-    cookiecutter("cookiecutters/project")
+    cookiecutter_path = get_cookiecutter_path("project")
+    cookiecutter(cookiecutter_path)
 
 
 @new_app.command("model")
 def new_model():
     typer.echo("Creating new model...")
     try:
-        cookiecutter("cookiecutters/model")
+        cookiecutter_path = get_cookiecutter_path("model")
+        cookiecutter(cookiecutter_path)
     except Exception as e:
         typer.echo(e)
 
