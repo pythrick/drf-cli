17d0d7534b9cb4c0b7314fcb682d00c0a62fa2c8
Patrick <patrick.pwall@gmail.com>
2020-10-06 20:03:51 -0300

    Fix module name

--- cookiecutters/project/{{cookiecutter.module_name}}/{{cookiecutter.module_name}}/app/viewsets/user.py
+++ cookiecutters/project/{{cookiecutter.module_name}}/{{cookiecutter.module_name}}/app/viewsets/user.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from pizzaria.app.serializers import UserSerializer
+from {{cookiecutter.module_name}}.app.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
--- cookiecutters/project/{{cookiecutter.module_name}}/{{cookiecutter.module_name}}/urls.py
+++ cookiecutters/project/{{cookiecutter.module_name}}/{{cookiecutter.module_name}}/urls.py
@@ -1,4 +1,4 @@
-"""pizzaria URL Configuration
+"""{{cookiecutter.module_name}} URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
